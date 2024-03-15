5277707ae38614885d4ba96420531215832417e5
orhan.calik <orhan.calik@student.ap.be>
2024-03-15 21:52:15 +0100

    JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-
+const pokemon1Stats = {
+    hp: 100,
+    attack: 20,
+    defense: 15
+  };
+  
+  const pokemon2Stats = {
+    hp: 100,
+    attack: 18,
+    defense: 12
+  };
+  
+  // Functie om het gevecht te starten
+  function startBattle() {
+    alert("Het gevecht begint!");
+  }
+  
+  // Functie om aan te vallen
+  function pokemonAttack() {
+    // Bereken de schade
+    const damage = pokemon1Stats.attack - pokemon2Stats.defense;
+    
+    // Verminder de HP van Pokémon 2
+    pokemon2Stats.hp -= damage;
+    
+    // Controleer of Pokémon 2 nog leeft
+    if (pokemon2Stats.hp <= 0) {
+      alert("Pokemon 1 wint!");
+    }
+  }
